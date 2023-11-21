bbd8bb72874c6aaabfd040f1e0d4fbd103d8a387
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-11-21 20:45:14 -0300

    tests

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,14 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import {  BrowserRouter, Route, Routes } from 'react-router-dom';
+// import {  BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' Component={App} />
-      <Route path='/:channel' Component={App} />
-    </Routes>
-  </BrowserRouter>
+  // <BrowserRouter>
+  //   <Routes>
+  //     <Route path='/' Component={App} />
+  //     <Route path='/:channel' Component={App} />
+  //   </Routes>
+  // </BrowserRouter>
+  <App />
 );
