4233b94b0bec95c7b7297a40ab37b9fee43015e1
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-11-21 20:29:53 -0300

    added react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import {  BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' Component={App} />
+      <Route path='/:channel' Component={App} />
+    </Routes>
+  </BrowserRouter>
 );
