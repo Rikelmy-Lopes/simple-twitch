629364dce353b49e6fc30a2e40d7bcea6dd033b0
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-11-22 16:50:57 -0300

    refactor: removido componente VideoChat

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 
 import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import { LiveChat } from './Pages/LiveChat';
 import { Chat } from './Pages/Chat';
 import { Live } from './Pages/Live';
 import { Embed } from './Pages/Embed';
@@ -11,14 +10,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' Component={LiveChat} />
-        <Route path='/:channel' Component={LiveChat} />
-        <Route path='/chat' Component={Chat} />
-        <Route path='/chat/:channel' Component={Chat} />
+        <Route path='/' Component={Embed} />
+        <Route path='/:channel' Component={Embed} />
         <Route path='/live' Component={Live} />
         <Route path='/live/:channel' Component={Live} />
-        <Route path='/embed' Component={Embed} />
-        <Route path='/embed/:channel' Component={Embed} />
+        <Route path='/chat' Component={Chat} />
+        <Route path='/chat/:channel' Component={Chat} />
       </Routes>
     </BrowserRouter>
   );
